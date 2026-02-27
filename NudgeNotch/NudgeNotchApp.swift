4e73819ad5cc90cd625dbb34659af3b977413150
suryanshkushwaha <suryanshkushwaha@u.nus.edu>
2026-02-27 11:16:13 +0800

    Initial Commit

---
+++ NudgeNotch/NudgeNotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NudgeNotchApp.swift
+//  NudgeNotch
+//
+//  Created by Suryansh Kushwaha on 27/2/26.
+//
+
+import SwiftUI
+
+@main
+struct NudgeNotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
